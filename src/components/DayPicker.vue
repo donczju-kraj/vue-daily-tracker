89aa9e0f14735d5d51eae44c1633b59891215ad9
Krzysztof Kraj <kraj.m.krzysztof@gmail.com>
2025-02-12 22:00:56 +0100

    Added button component, removed some global styles

--- src/components/DayPicker.vue
+++ src/components/DayPicker.vue
@@ -1,18 +1,19 @@
 <template>
-  <div>
-    <button @click="prevDay">Previous day</button>
-    <span>{{ currentDayFormatted }}</span>
-    <button @click="nextDay">Next day</button>
+  <div class="mt-4 flex items-center space-x-3 justify-between">
+    <Button @click="prevDay">Previous day</Button>
+    <span class="font-semibold text-sm">{{ currentDayFormatted }}</span>
+    <Button @click="nextDay">Next day</Button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue';
+import Button from './commons/Button.vue';
 
 const currentDay = ref(new Date());
 
 const currentDayFormatted = computed(() => {
-  return currentDay.value.toLocaleDateString(); // Formatowanie daty
+  return currentDay.value.toLocaleDateString();
 });
 
 const emit = defineEmits(['day-changed']);
@@ -27,7 +28,3 @@ const nextDay = () => {
   emit('day-changed', new Date(currentDay.value));
 };
 </script>
-
-<style scoped>
-/* Dodaj style dla komponentu DayPicker */
-</style>
---
+++ src/components/commons/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button 
+    @click="$emit('click')" 
+    class="px-3 py-2 bg-gray-200 text-gray-800 text-sm font-semibold text-center rounded-full hover:font-bold transition ease-in-out duration-300 delay-150 hover:scale-105">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+const emit = defineEmits(['click']);
+
+</script>
